528199bc6fa24cb05f3eef8aba37ff4b1a7ee2d9
Vivek Nagesh <56115917+viveknagesh1@users.noreply.github.com>
2021-09-22 11:30:16 +0530

    Update app.py

--- Files/app.py
+++ Files/app.py
@@ -68,8 +68,8 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# app.config["MONGO_URI"] = "mongodb+srv://viveknagesh1:Vivek0935@ltts.jjzpl.mongodb.net/ltts?retryWrites=true&w=majority"
-app.config["MONGO_URI"] = "mongodb://localhost:27017/ltts"
+app.config["MONGO_URI"] = "mongodb+srv://viveknagesh1:Vivek0935@ltts.jjzpl.mongodb.net/ltts?retryWrites=true&w=majority"
+#app.config["MONGO_URI"] = "mongodb://localhost:27017/ltts"
 
 mongo = PyMongo(app)
 db = mongo.db
